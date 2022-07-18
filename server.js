6bcaebd389c5f64b3db5a75718a943c2f90e8307
Luna R <snowluna@protonmail.com>
2022-07-18 03:07:11 +0000

    add gitignore and server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+// start
+const app = express();
+
+app.listen(3000)
